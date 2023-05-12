2ca601216f72e2c7523292f9f19f984f2f0dac5b
M M Salman <76009859+MMSalman26@users.noreply.github.com>
2023-05-12 10:16:42 +0000

    fifth commit

--- myapp/pages/index.js
+++ myapp/pages/index.js
@@ -8,14 +8,17 @@ export default function Home() {
   return (
    <>
    <Head>
-   <title>Mizan App</title>
-        <meta name="description" content="This App is created for exposing the goddamned freak Mizan" />
+   <title>Mizan App | bxd mizan</title>
+        <meta name="description" content="This App is created for exposing the bxd mizan" />
         <meta property="og:type" content="website"></meta>
         <meta property="og:title" content="Mizan App" />
-        <meta property="og:description" content="This App is created for exposing the goddamned freak Mizan" />
+        <meta property="og:description" content="This App is created for exposing the bxd mizan" />
         <meta property="og:image" content="https://www.shutterstock.com/shutterstock/photos/699192727/display_1500/stock-photo-portrait-very-fat-man-raises-a-dumbbell-morning-exercise-of-a-fat-man-with-dumbbells-white-699192727.jpg" />
    </Head>
-   <div className='h-screen w-screen bg-slate-900 flex justify-center items-center text-cyan-300 text-3xl'>Mizan Akta Bokchod</div>
+   <div className='h-screen w-screen bg-slate-900 flex justify-center items-center text-cyan-300 text-3xl'>
+    <h1>bxd mizan</h1>
+    <h2 className='my-20'>bxd mizan</h2>
+   </div>
    </>
   )
 }
